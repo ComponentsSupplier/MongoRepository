a18b33a81ca9ccb50306244a6a4cf67c25bb714c
Walter Pinson <walter@alekto.co>
2017-11-29 08:51:28 -0500

    Resolve all compile-time errors. Delete anything that was deprecated.  Refactor methods that need to go about doing their business differently with the latest version of the async driver.

--- MongoRepository.NetCore/RepositoryManager/IRepositoryManager.cs
+++ MongoRepository.NetCore/RepositoryManager/IRepositoryManager.cs
@@ -1,4 +1,4 @@
-﻿namespace MongoRepository
+﻿namespace MongoRepository.NetCore
 {
     using System.Collections.Generic;
     using MongoDB.Driver;
@@ -53,110 +53,10 @@
         void DropAllIndexes();
 
         /// <summary>
-        /// Ensures that the desired index exist and creates it if it doesn't exist.
-        /// </summary>
-        /// <param name="keyname">The indexed field.</param>
-        /// <remarks>
-        /// This is a convenience method for EnsureIndexes(IMongoIndexKeys keys, IMongoIndexOptions options).
-        /// Index will be ascending order, non-unique, non-sparse.
-        /// </remarks>
-        void EnsureIndex(string keyname);
-
-        /// <summary>
-        /// Ensures that the desired index exist and creates it if it doesn't exist.
-        /// </summary>
-        /// <param name="keyname">The indexed field.</param>
-        /// <param name="descending">Set to true to make index descending, false for ascending.</param>
-        /// <param name="unique">Set to true to ensure index enforces unique values.</param>
-        /// <param name="sparse">Set to true to specify the index is sparse.</param>
-        /// <remarks>
-        /// This is a convenience method for EnsureIndexes(IMongoIndexKeys keys, IMongoIndexOptions options).
-        /// </remarks>
-        void EnsureIndex(string keyname, bool descending, bool unique, bool sparse);
-
-        /// <summary>
-        /// Ensures that the desired indexes exist and creates them if they don't exist.
-        /// </summary>
-        /// <param name="keynames">The indexed fields.</param>
-        /// <remarks>
-        /// This is a convenience method for EnsureIndexes(IMongoIndexKeys keys, IMongoIndexOptions options).
-        /// Index will be ascending order, non-unique, non-sparse.
-        /// </remarks>
-        void EnsureIndexes(IEnumerable<string> keynames);
-
-        /// <summary>
-        /// Ensures that the desired indexes exist and creates them if they don't exist.
-        /// </summary>
-        /// <param name="keynames">The indexed fields.</param>
-        /// <param name="descending">Set to true to make index descending, false for ascending.</param>
-        /// <param name="unique">Set to true to ensure index enforces unique values.</param>
-        /// <param name="sparse">Set to true to specify the index is sparse.</param>
-        /// <remarks>
-        /// This is a convenience method for EnsureIndexes(IMongoIndexKeys keys, IMongoIndexOptions options).
-        /// </remarks>
-        void EnsureIndexes(IEnumerable<string> keynames, bool descending, bool unique, bool sparse);
-
-        /// <summary>
-        /// Ensures that the desired indexes exist and creates them if they don't exist.
-        /// </summary>
-        /// <param name="keys">The indexed fields.</param>
-        /// <param name="options">The index options.</param>
-        /// <remarks>
-        /// This is a convenience method for EnsureIndexes(IMongoIndexKeys keys, IMongoIndexOptions options).
-        /// Index will be ascending order, non-unique, non-sparse.
-        /// </remarks>
-        void EnsureIndexes(IMongoIndexKeys keys, IMongoIndexOptions options);
-
-        /// <summary>
-        /// Tests whether indexes exist.
-        /// </summary>
-        /// <param name="keyname">The indexed fields.</param>
-        /// <returns>Returns true when the indexes exist, false otherwise.</returns>
-        bool IndexExists(string keyname);
-
-        /// <summary>
-        /// Tests whether indexes exist.
-        /// </summary>
-        /// <param name="keynames">The indexed fields.</param>
-        /// <returns>Returns true when the indexes exist, false otherwise.</returns>
-        bool IndexesExists(IEnumerable<string> keynames);
-
-        /// <summary>
-        /// Runs the ReIndex command on this repository.
-        /// </summary>
-        void ReIndex();
-
-        /// <summary>
-        /// Gets the total size for the repository (data + indexes).
-        /// </summary>
-        /// <returns>Returns total size for the repository (data + indexes).</returns>
-        long GetTotalDataSize();
-
-        /// <summary>
-        /// Gets the total storage size for the repository (data + indexes).
-        /// </summary>
-        /// <returns>Returns total storage size for the repository (data + indexes).</returns>
-        long GetTotalStorageSize();
-
-        /// <summary>
-        /// Validates the integrity of the repository.
-        /// </summary>
-        /// <returns>Returns a ValidateCollectionResult.</returns>
-        /// <remarks>You will need to reference MongoDb.Driver.</remarks>
-        ValidateCollectionResult Validate();
-
-        /// <summary>
-        /// Gets stats for this repository.
-        /// </summary>
-        /// <returns>Returns a CollectionStatsResult.</returns>
-        /// <remarks>You will need to reference MongoDb.Driver.</remarks>
-        CollectionStatsResult GetStats();
-
-        /// <summary>
         /// Gets the indexes for this repository.
         /// </summary>
         /// <returns>Returns the indexes for this repository.</returns>
-        GetIndexesResult GetIndexes();
+        IMongoIndexManager<T> GetIndexes();
     }
 
     /// <summary>
--- MongoRepository.NetCore/RepositoryManager/MongoRepositoryManager.cs
+++ MongoRepository.NetCore/RepositoryManager/MongoRepositoryManager.cs
@@ -1,7 +1,7 @@
-﻿namespace MongoRepository
+﻿namespace MongoRepository.NetCore
 {
     using MongoDB.Driver;
-    using MongoDB.Driver.Builders;
+    // using MongoDB.Driver.Builders;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -27,7 +27,7 @@
         /// <summary>
         /// MongoCollection field.
         /// </summary>
-        private MongoCollection<T> collection;
+        private IMongoCollection<T> collection;
 
         /// <summary>
         /// Initializes a new instance of the MongoRepositoryManager class.
@@ -64,7 +64,8 @@
         /// <value>Returns true when the collection already exists, false otherwise.</value>
         public virtual bool Exists
         {
-            get { return this.collection.Exists(); }
+            get { throw new NotImplementedException(); }
+            // get { return this.collection.Exists(); }
         }
 
         /// <summary>
@@ -73,7 +74,7 @@
         /// <value>The name of the collection as Mongo uses.</value>
         public virtual string Name
         {
-            get { return this.collection.Name; }
+            get { return this.collection.CollectionNamespace.CollectionName; }
         }
 
         /// <summary>
@@ -81,7 +82,7 @@
         /// </summary>
         public virtual void Drop()
         {
-            this.collection.Drop();
+            this.collection.Database.DropCollection(this.Name);
         }
 
         /// <summary>
@@ -90,7 +91,8 @@
         /// <returns>Returns true when the repository is capped, false otherwise.</returns>
         public virtual bool IsCapped()
         {
-            return this.collection.IsCapped();
+            throw new NotImplementedException();
+            // return this.collection.IsCapped();
         }
 
         /// <summary>
@@ -99,7 +101,8 @@
         /// <param name="keyname">The name of the indexed field.</param>
         public virtual void DropIndex(string keyname)
         {
-            this.DropIndexes(new string[] { keyname });
+            this.collection.Indexes.DropOne(keyname);
+            this.collection.Indexes.List().ToList();
         }
 
         /// <summary>
@@ -108,7 +111,10 @@
         /// <param name="keynames">The names of the indexed fields.</param>
         public virtual void DropIndexes(IEnumerable<string> keynames)
         {
-            this.collection.DropIndex(keynames.ToArray());
+            foreach(var keyname in keynames)
+            {
+                this.DropIndex(keyname);
+            }
         }
 
         /// <summary>
@@ -116,165 +122,16 @@
         /// </summary>
         public virtual void DropAllIndexes()
         {
-            this.collection.DropAllIndexes();
-        }
-
-        /// <summary>
-        /// Ensures that the desired index exist and creates it if it doesn't exist.
-        /// </summary>
-        /// <param name="keyname">The indexed field.</param>
-        /// <remarks>
-        /// This is a convenience method for EnsureIndexes(IMongoIndexKeys keys, IMongoIndexOptions options).
-        /// Index will be ascending order, non-unique, non-sparse.
-        /// </remarks>
-        public virtual void EnsureIndex(string keyname)
-        {
-            this.EnsureIndexes(new string[] { keyname });
-        }
-
-        /// <summary>
-        /// Ensures that the desired index exist and creates it if it doesn't exist.
-        /// </summary>
-        /// <param name="keyname">The indexed field.</param>
-        /// <param name="descending">Set to true to make index descending, false for ascending.</param>
-        /// <param name="unique">Set to true to ensure index enforces unique values.</param>
-        /// <param name="sparse">Set to true to specify the index is sparse.</param>
-        /// <remarks>
-        /// This is a convenience method for EnsureIndexes(IMongoIndexKeys keys, IMongoIndexOptions options).
-        /// </remarks>
-        public virtual void EnsureIndex(string keyname, bool descending, bool unique, bool sparse)
-        {
-            this.EnsureIndexes(new string[] { keyname }, descending, unique, sparse);
-        }
-
-        /// <summary>
-        /// Ensures that the desired indexes exist and creates them if they don't exist.
-        /// </summary>
-        /// <param name="keynames">The indexed fields.</param>
-        /// <remarks>
-        /// This is a convenience method for EnsureIndexes(IMongoIndexKeys keys, IMongoIndexOptions options).
-        /// Index will be ascending order, non-unique, non-sparse.
-        /// </remarks>
-        public virtual void EnsureIndexes(IEnumerable<string> keynames)
-        {
-            this.EnsureIndexes(keynames, false, false, false);
-        }
-
-        /// <summary>
-        /// Ensures that the desired indexes exist and creates them if they don't exist.
-        /// </summary>
-        /// <param name="keynames">The indexed fields.</param>
-        /// <param name="descending">Set to true to make index descending, false for ascending.</param>
-        /// <param name="unique">Set to true to ensure index enforces unique values.</param>
-        /// <param name="sparse">Set to true to specify the index is sparse.</param>
-        /// <remarks>
-        /// This is a convenience method for EnsureIndexes(IMongoIndexKeys keys, IMongoIndexOptions options).
-        /// </remarks>
-        public virtual void EnsureIndexes(IEnumerable<string> keynames, bool descending, bool unique, bool sparse)
-        {
-            var ixk = new IndexKeysBuilder();
-            if (descending)
-            {
-                ixk.Descending(keynames.ToArray());
-            }
-            else
-            {
-                ixk.Ascending(keynames.ToArray());
-            }
-
-            this.EnsureIndexes(
-                ixk,
-                new IndexOptionsBuilder().SetUnique(unique).SetSparse(sparse));
-        }
-
-        /// <summary>
-        /// Ensures that the desired indexes exist and creates them if they don't exist.
-        /// </summary>
-        /// <param name="keys">The indexed fields.</param>
-        /// <param name="options">The index options.</param>
-        /// <remarks>
-        /// This method allows ultimate control but does "leak" some MongoDb specific implementation details.
-        /// </remarks>
-        public virtual void EnsureIndexes(IMongoIndexKeys keys, IMongoIndexOptions options)
-        {
-            this.collection.CreateIndex(keys, options);
-        }
-
-        /// <summary>
-        /// Tests whether indexes exist.
-        /// </summary>
-        /// <param name="keyname">The indexed fields.</param>
-        /// <returns>Returns true when the indexes exist, false otherwise.</returns>
-        public virtual bool IndexExists(string keyname)
-        {
-            return this.IndexesExists(new string[] { keyname });
-        }
-
-        /// <summary>
-        /// Tests whether indexes exist.
-        /// </summary>
-        /// <param name="keynames">The indexed fields.</param>
-        /// <returns>Returns true when the indexes exist, false otherwise.</returns>
-        public virtual bool IndexesExists(IEnumerable<string> keynames)
-        {
-            return this.collection.IndexExists(keynames.ToArray());
-        }
-
-        /// <summary>
-        /// Runs the ReIndex command on this repository.
-        /// </summary>
-        public virtual void ReIndex()
-        {
-            this.collection.ReIndex();
-        }
-
-        /// <summary>
-        /// Gets the total size for the repository (data + indexes).
-        /// </summary>
-        /// <returns>Returns total size for the repository (data + indexes).</returns>
-        [Obsolete("This method will be removed in the next version of the driver")]
-        public virtual long GetTotalDataSize()
-        {
-            return this.collection.GetTotalDataSize();
-        }
-
-        /// <summary>
-        /// Gets the total storage size for the repository (data + indexes).
-        /// </summary>
-        /// <returns>Returns total storage size for the repository (data + indexes).</returns>
-        [Obsolete("This method will be removed in the next version of the driver")]
-        public virtual long GetTotalStorageSize()
-        {
-            return this.collection.GetTotalStorageSize();
-        }
-
-        /// <summary>
-        /// Validates the integrity of the repository.
-        /// </summary>
-        /// <returns>Returns a ValidateCollectionResult.</returns>
-        /// <remarks>You will need to reference MongoDb.Driver.</remarks>
-        public virtual ValidateCollectionResult Validate()
-        {
-            return this.collection.Validate();
-        }
-
-        /// <summary>
-        /// Gets stats for this repository.
-        /// </summary>
-        /// <returns>Returns a CollectionStatsResult.</returns>
-        /// <remarks>You will need to reference MongoDb.Driver.</remarks>
-        public virtual CollectionStatsResult GetStats()
-        {
-            return this.collection.GetStats();
+            this.collection.Indexes.DropAll();
         }
 
         /// <summary>
         /// Gets the indexes for this repository.
         /// </summary>
         /// <returns>Returns the indexes for this repository.</returns>
-        public virtual GetIndexesResult GetIndexes()
+        public IMongoIndexManager<T> GetIndexes()
         {
-            return this.collection.GetIndexes();
+            return this.collection.Indexes;
         }
     }
 
